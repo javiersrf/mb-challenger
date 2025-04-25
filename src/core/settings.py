3fbdf3cbe3498105e324210ffbf3fb285917b9fe
javiersrf <javiersrf@gmail.com>
2025-04-25 18:59:54 -0300

    chore: env sample and new config

--- src/core/settings.py
+++ src/core/settings.py
@@ -5,11 +5,10 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        case_sensitive=True,
     )
 
     DATABASE_URL: str
-    SECRET_KEY: str
+    MB_URL: str
 
     class Config:
         env_file = ".env"
